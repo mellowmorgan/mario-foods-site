116642cb478411988f076b258da52b04a50f6e76
mellowmorgan <morgan.waites@gmail.com>
2022-01-07 11:00:36 -0800

    edit views an controller reviews add

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,52 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @product = Product.find(params[:product_id]) 
+    @review = @product.reviews.new
+    render :new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Review added successfully!"
+      redirect_to product_path(@product)
+    else
+
+      flash[:alert] = "There was an error in creating your review."
+      render :new
+    end
+  end
+
+  # def edit
+  #   @review = Review.find(params[:id])
+  #   render :edit
+  # end
+
+  # def show
+  #   @review = Review.find(params[:id])
+  #   render :show
+  # end
+
+  # def update
+  #   @review = Review.find(params[:id])
+  #   if @review.update(review_params)
+  #     flash[:notice] = "review update success!"
+  #     redirect_to reviews_path
+  #   else
+  #     flash[:alert] = "There was an error in updating your review."
+  #     render :edit
+  #   end
+  # end
+  # def destroy
+  #   @review = Review.find(params[:id])
+  #   @review.destroy
+  #   redirect_to reviews_path
+  # end
+
+  private
+    def review_params
+      params.require(:review).permit(:author, :rating, :content_body)
+    end
+end
