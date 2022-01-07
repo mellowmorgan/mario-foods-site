6d6b7fe798704ff5448be02c282fa3c3ad31c9be
mellowmorgan <morgan.waites@gmail.com>
2022-01-07 09:39:08 -0800

    add tests and code for validation content_body length

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,6 @@ class Review < ApplicationRecord
   validates :content_body, presence: true
   validates :rating, presence: true
   validates :rating, :inclusion => 1..5
+  validates_length_of :content_body, minimum: 50
+  validates_length_of :content_body, maximum: 250
 end
