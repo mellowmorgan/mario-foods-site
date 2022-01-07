a73ef9eefe9fb1a54bf2e520718e09935ccb555f
mellowmorgan <morgan.waites@gmail.com>
2022-01-07 09:32:56 -0800

    add test in spec for review rating range validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ApplicationRecord
   validates :author, presence: true
   validates :content_body, presence: true
   validates :rating, presence: true
+  validates :rating, :inclusion => 1..5
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,4 +5,5 @@ describe Review do
   it { should validate_presence_of :author }
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
  end
