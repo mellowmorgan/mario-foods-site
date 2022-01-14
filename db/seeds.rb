ec46d0a59bee218f9c013e22f560c9528d05e0c0
mellowmorgan <morgan.waites@gmail.com>
2022-01-14 14:36:00 -0800

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,15 +19,15 @@ end
 p1 = Product.find(1)
 p1.country_of_origin = "United States of America"
 p1.save
-Product.find(2)
+p2 = Product.find(2)
 p2.country_of_origin = "United States of America"
 p2.save
-Product.find(3)
+p3 = Product.find(3)
 p3.country_of_origin = "United States of America"
 p3.save
 
 #give a product one more review to make it show on homepage "most review"
-p1.reviews.create!({author: Faker::Name.name, product_id: product.id, rating: Faker::Number.within(range: 1..5), content_body: Faker::Lorem.paragraph_by_chars(number: 100)})
+p1.reviews.create!({author: Faker::Name.name, product_id: p1.id, rating: Faker::Number.within(range: 1..5), content_body: Faker::Lorem.paragraph_by_chars(number: 100)})
 
 User.create!({email:"test@test.com", password:"testing", password_confirmation:"testing"})
 User.create!({email:"admin@test.com", password:"testing", password_confirmation:"testing", admin:true})
