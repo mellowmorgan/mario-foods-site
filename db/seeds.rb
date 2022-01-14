bb83aa78339f9e2e7fe3dccbb03f9ab61eb505f2
mellowmorgan <morgan.waites@gmail.com>
2022-01-14 14:26:06 -0800

    add code to seeds to make USA, most reviews products

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,20 @@ User.destroy_all
     Review.create!({author: Faker::Name.name, product_id: product.id, rating: Faker::Number.within(range: 1..5), content_body: Faker::Lorem.paragraph_by_chars(number: 100) })
   end
 end
+#set some usa products for homepage display
+p1 = Product.find(1)
+p1.country_of_origin = "United States of America"
+p1.save
+Product.find(2)
+p2.country_of_origin = "United States of America"
+p2.save
+Product.find(3)
+p3.country_of_origin = "United States of America"
+p3.save
+
+#give a product one more review to make it show on homepage "most review"
+p1.reviews.create!({author: Faker::Name.name, product_id: product.id, rating: Faker::Number.within(range: 1..5), content_body: Faker::Lorem.paragraph_by_chars(number: 100)})
+
 User.create!({email:"test@test.com", password:"testing", password_confirmation:"testing"})
 User.create!({email:"admin@test.com", password:"testing", password_confirmation:"testing", admin:true})
 
